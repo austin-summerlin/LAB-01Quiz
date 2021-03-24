3b9666f28ef3c7f1677b7f76ee4a6e3793f493fa
Austin Summerlin <summerlin.austin@gmail.com>
2021-03-24 09:43:11 -0700

    Fix Tests

--- test/example.test.js
+++ test/example.test.js
@@ -1,21 +1,20 @@
-// IMPORT MODULES under test here:
-// import { example } from '../example.ts';
-import { isYes } from './utils.js';
+import { isYes } from '../utils.js';
 
 const test = QUnit.test;
 
 test('should take in a string and return true if answer begins with a y', (expect) => {
     //Arrange
     // Set up your arguments and expectations
+    const userInput = 'yes';
     const expected = true;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = isYes('yes');
+    const actual = isYes(userInput);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected, 'yes returns true');
-    expect.equal(isYes('YAASSS'), true, 'YAASSS returns true');
-    expect.equal(isYes('NOOOOOOO'), false, 'NOOOOOOO returns false');
+    expect.equal(actual, expected, userInput, 'yes returns true');
+    expect.equal(isYes('YAS'), true, 'YAS returns true');
+    expect.equal(isYes('NOO'), false, 'NOO returns false');
 });
